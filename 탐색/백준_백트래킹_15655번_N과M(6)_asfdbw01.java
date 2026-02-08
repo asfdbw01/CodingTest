34b9d7f57586824abfee784d6b11f60ea0e26b0e
asfdbw01 <149449548+asfdbw01@users.noreply.github.com>
2026-02-08 15:05:29 +0900

    [탐색] N과M(6) - 백트래킹

---
+++ 탐색/백준_백트래킹_15655번_N과M(6)_asfdbw01.java
@@ -0,0 +1,69 @@
+/*
+문제
+N개의 자연수와 자연수 M이 주어졌을 때, 
+아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. 
+N개의 자연수는 모두 다른 수이다.
+
+N개의 자연수 중에서 M개를 고른 수열
+고른 수열은 오름차순이어야 한다.
+
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 
+각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+*/
+
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int[] arr;
+    static StringBuilder out = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        makeArr(N, br);
+        Arrays.sort(arr);
+        int[] pick = new int[M];
+        boolean[] visited = new boolean[N];
+        backTracking(N, M, pick, visited, 0, 0);
+        System.out.println(out.toString());
+    }
+
+    private static void backTracking(int N,int M,int[] pick,boolean[] visited,int depth,int start){
+        if(depth == M ){
+            for(int i=0;i<M;i++)out.append(pick[i]).append(" ");
+            out.append("\n");
+            return;
+        }
+
+        //int prev = Integer.MIN_VALUE;
+        for(int i=start;i<N;i++){
+            if(visited[i]==true)continue;
+            //if(prev == arr[i])continue;
+            //prev = arr[i];
+            visited[i] = true;
+            pick[depth] = arr[i];
+            backTracking(N, M, pick, visited, depth+1,i+1);
+            visited[i]=false;
+        }
+    }
+
+    private static void makeArr(int N,BufferedReader br) throws IOException {
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++)arr[i] = Integer.parseInt(st.nextToken());
+    }
+}
